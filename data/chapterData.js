668a02fdae4c03ac3ede725eb9008321b4c9b4cd
maigotan <maigotan@users.noreply.github.com>
2018-07-12 22:01:20 -0700

    kannou 12 rls

--- data/chapterData.js
+++ data/chapterData.js
@@ -4,40 +4,34 @@ window.chapterData = {
       '47' : {
         date: new Date('July 05 2018 19:30:00 GMT-0700'),
         src: '/img/releases/mahoyo/47',
-        trans: 'AnonSpore',
         let: 'Nielle',
         red: 'myran, kukupa, Crosshairs',
       },
       '46' : {
         date: new Date('May 20 2018 13:30:00 GMT-0700'),
         src: '/img/releases/mahoyo/46',
-        trans: 'AnonSpore',
         let: 'Nielle',
         red: 'myran'
       }, 
       '45' : {
         date: new Date('May 2 2018 23:25:00 GMT-0700'),
         src: '/img/releases/mahoyo/45',
-        trans: 'AnonSpore',
         let: 'Nielle',
         red: 'myran'
       },      
       '44' : {
         date: new Date('Mar 13 2018 19:45:00 GMT-0800'),
         src: '/img/releases/mahoyo/44',
-        trans: 'AnonSpore',
         let: 'Nielle'
       },
       '43' : {
         date: new Date('Feb 12 2018 19:26:00 GMT-0800'),
         src: '/img/releases/mahoyo/43',
-        trans: 'AnonSpore',
         let: 'Nielle'
       },
       '42' : {
         date: new Date('Dec 19 2017 00:00:00 GMT-0700'),
         src: '/img/releases/mahoyo/42',
-        trans: 'AnonSpore',
         let: 'Nielle'
       }
     },
@@ -45,133 +39,111 @@ window.chapterData = {
       '22': {
         date: new Date('Jan 30 2018 22:20:00 GMT-0800'),
         src: '/img/releases/mahoromi/22',
-        trans: 'AnonSpore',
         let: 'Nielle'
       },
       '21': {
         date: new Date('Nov 17 2017 21:05:00 GMT-0700'),
         src: '/img/releases/mahoromi/21',
-        trans: 'AnonSpore',
         let: 'Nielle'
       },
       // '20': {
       //   date: new Date('Sep 21 2017 19:00:00 GMT-0700'),
       //   src: 'http://img.bato.to/comics/2017/09/22/m/read59c46f134f135',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '19': {
       //   date: new Date('Sep 17 2017 18:32:00 GMT-0700'),
       //   src: 'http://img.bato.to/comics/2017/09/18/m/read59bf2151be533',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '18': {
       //   date: new Date('Jul 10 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/07/10/m/read5963187462456',
-      //   trans: 'AnonSpore',
       //   let: 'cranon'
       // },
       // '17': {
       //   date: new Date('Jun 13 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/06/14/m/read5940a3b165763',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '16': {
       //   date: new Date('Mar 28 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/03/29/m/read58db1fee39584',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '15': {
       //   date: new Date('Mar 27 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/03/28/m/read58d9f96682d15',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '14': {
       //   date: new Date('Mar 09 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/03/10/m/read58c24c598c4a6',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '13': {
       //   date: new Date('Feb 22 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/02/23/m/read58ae5ddfcf589',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '12': {
       //   date: new Date('Feb 09 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/02/10/m/read589d5f1fef8a9',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '11': {
       //   date: new Date('Jan 26 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/01/26/m/read5889a4b940538',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '10': {
       //   date: new Date('Jan 18 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/01/18/m/read587f185ddf4de',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '9': {
       //   date: new Date('Jan 14 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/01/14/m/read5879eab73d38f',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '8': {
       //   date: new Date('Jan 13 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/01/13/m/read58788ae72bacf',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '7': {
       //   date: new Date('Jan 11 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/01/11/m/read5876658488be0',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '6': {
       //   date: new Date('Jan 07 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/01/07/m/read58709d77824fd',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '5': {
       //   date: new Date('Jan 05 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/01/05/m/read586e050a85474',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '4': {
       //   date: new Date('Jan 04 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/01/04/m/read586cc6ab51029',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '3': {
       //   date: new Date('Jan 04 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/01/04/m/read586cc676591df',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '2': {
       //   date: new Date('Dec 30 2016 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2016/12/31/m/read586715cc7dad7',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '1': {
       //   date: new Date('Dec 30 2016 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/01/04/m/read586cc6569d44e',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // }
     },
@@ -179,36 +151,37 @@ window.chapterData = {
       '23' : {
         date: new Date('Mar 24 2018 12:40 GMT-0700'),
         src: '/img/releases/spoflo/23',
-        trans: 'AnonSpore',
         let: 'Cranon',
       },
       '22' : {
         date: new Date('Mar 17 2018 20:06 GMT-0700'),
         src: '/img/releases/spoflo/22',
-        trans: 'AnonSpore',
         let: 'Kiiragi',
       },
       '21.5' : {
         date: new Date('Oct 10 2017 20:00 GMT-0700'),
         src: '/img/releases/spoflo/21_5',
-        trans: 'AnonSpore',
         let: 'Nielle',
         red: 'Nami',
       },
       // '21' : {
       //   date: new Date('Aug 12 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/08/12/s/read598ebee1c778b',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // },
       // '20.5' : {
       //   date: new Date('Jul 06 2017 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2017/06/03/s/read593278754ca38',
-      //   trans: 'AnonSpore',
       //   let: 'cranon'
       // }
     },
     'Kannou Sensei' : {
+      '12': {
+        date: new Date('July 12 2018 21:47:00 GMT-0800'),
+        src: '/img/releases/kannou/12',
+        trans: 'Haremless', 
+        let: 'cranon'
+      },
       '11': {
         date: new Date('July 03 2018 22:00:00 GMT-0800'),
         src: '/img/releases/kannou/11',
@@ -446,7 +419,6 @@ window.chapterData = {
       // '1': {
       //   date: new Date('Dec 07 2015 GMT-0800'),
       //   src: 'http://img.bato.to/comics/2015/12/08/a/read566671fd9606e',
-      //   trans: 'AnonSpore',
       //   let: 'Nielle'
       // }
     },
