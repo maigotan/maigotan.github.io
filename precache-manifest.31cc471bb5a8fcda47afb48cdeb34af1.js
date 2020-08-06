ac348983c66b5218241caab9bf0f85c291618c23
maigotan <maigotan@users.noreply.github.com>
2020-08-05 21:51:04 -0700

    scroll up on new page, remove disqus from reader

---
+++ precache-manifest.31cc471bb5a8fcda47afb48cdeb34af1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271f7ba112cd7b80bfcaeaab402ff326",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38ef18069f48eea63a33",
+    "url": "/static/css/2.3d2d96e3.chunk.css"
+  },
+  {
+    "revision": "91adb84adb29d2ce7cad",
+    "url": "/static/css/main.a95c53fc.chunk.css"
+  },
+  {
+    "revision": "38ef18069f48eea63a33",
+    "url": "/static/js/2.349d4c6d.chunk.js"
+  },
+  {
+    "revision": "a3ef6a11e11ca6a16b435c3d2be41ee9",
+    "url": "/static/js/2.349d4c6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91adb84adb29d2ce7cad",
+    "url": "/static/js/main.09b5056e.chunk.js"
+  },
+  {
+    "revision": "a6a1e028e8548f0c2403",
+    "url": "/static/js/runtime-main.268fa134.js"
+  },
+  {
+    "revision": "41484d92c876f76b20c7f746221e8151",
+    "url": "/static/media/discord-icon.41484d92.svg"
+  }
+]);
